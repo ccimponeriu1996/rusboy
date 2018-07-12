62c43cc4b4d16910a428c74a9de00a4645737c1c
Christopher Cimponeriu <ccimponeriu@gmail.com>
2018-07-12 12:04:57 -0400

    First commit. Can read files and print name.

---
+++ rusboy/target/debug/deps/rusboy-2a57c90d126f9715.d
@@ -0,0 +1,5 @@
+/home/ccimponeriu1996/projects/rusboy/target/debug/deps/rusboy-2a57c90d126f9715: src/main.rs
+
+/home/ccimponeriu1996/projects/rusboy/target/debug/deps/rusboy-2a57c90d126f9715.d: src/main.rs
+
+src/main.rs:
---
+++ rusboy/target/debug/rusboy.d
@@ -0,0 +1,1 @@
+/home/ccimponeriu1996/projects/rusboy/target/debug/rusboy: /home/ccimponeriu1996/projects/rusboy/src/main.rs
